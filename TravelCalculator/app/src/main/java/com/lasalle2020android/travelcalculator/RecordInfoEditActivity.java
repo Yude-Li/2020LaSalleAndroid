5a731612ce540cadfba5dc5a57bd53b19caf3ce4
evakung56412 <eva56412@gmail.com>
2020-04-02 00:39:39 -0400

    Revise name "recordedit" to "recordinfo"

--- TravelCalculator/app/src/main/java/com/lasalle2020android/travelcalculator/RecordInfoEditActivity.java
+++ TravelCalculator/app/src/main/java/com/lasalle2020android/travelcalculator/RecordInfoEditActivity.java
@@ -1,26 +1,26 @@
 package com.lasalle2020android.travelcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class RecordInfoEditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.trip_info_layout);
+        setContentView(R.layout.record_info_layout);
 
-        Toolbar mTopToolbar = findViewById(R.id.toolbar_recordedit);
+        Toolbar mTopToolbar = findViewById(R.id.toolbar_recordinfo);
         setSupportActionBar(mTopToolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.recordedit_toolbar_menu, menu);
+        getMenuInflater().inflate(R.menu.recordinfo_toolbar_menu, menu);
         return true;
     }
 
