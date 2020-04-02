02e90de655d7e9d841217482c9e150ec4d67fde7
evakung56412 <eva56412@gmail.com>
2020-04-02 00:42:14 -0400

    Create record list layout with actionbar (toolbar)

---
+++ TravelCalculator/app/src/main/java/com/lasalle2020android/travelcalculator/RecordListActivity.java
@@ -0,0 +1,48 @@
+package com.lasalle2020android.travelcalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class RecordListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.record_list_layout);
+
+        Toolbar mTopToolbar = findViewById(R.id.toolbar_recordlist);
+        setSupportActionBar(mTopToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Set the toolBar layout from menu
+        getMenuInflater().inflate(R.menu.recordlist_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Button action for actionBar(toolBar)
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_addRecord:
+            {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), RecordInfoEditActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+            break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
