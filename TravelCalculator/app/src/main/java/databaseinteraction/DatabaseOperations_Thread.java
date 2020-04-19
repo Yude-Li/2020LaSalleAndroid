58877258ca8a5686c76f94663c29d730f5c3e8d5
adityabugalia <aadibugalia@gmail.com>
2020-04-19 14:57:47 -0400

    The code  changes for progress dialog. The previous Native progress dialog class has been depricated. This new class is custom class to achive the same functionality.

--- TravelCalculator/app/src/main/java/databaseinteraction/DatabaseOperations_Thread.java
+++ TravelCalculator/app/src/main/java/databaseinteraction/DatabaseOperations_Thread.java
@@ -1,7 +1,10 @@
 package databaseinteraction;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
@@ -9,77 +12,77 @@ import Model.ExpenseModel;
 import Model.TripInfoModel;
 import callbacks.DatabaseOperationNotifier;
 import commonutilities.Constants;
+import commonutilities.ProgressBar_YAM;
 
 public class DatabaseOperations_Thread extends AsyncTask {
 
 
+    private Context mContext;
+    private AppCompatActivity mActivity;
+    private Constants.DATABSE_OPERATION mOperation_Name;
+    private Constants.TABLE mTableName;
+    private DatabaseOperationNotifier mDatabaseOperationNotifier;
+    private List<TripInfoModel> mAllTrips;
+    private List<ExpenseModel> mAllExpenses;
+    private ProgressBar_YAM mProgressDialog;
 
 
+    public DatabaseOperations_Thread(Context mContext) {
 
-private Context mContext;
-private Constants.DATABSE_OPERATION mOperation_Name;
-private Constants.TABLE mTableName;
-private DatabaseOperationNotifier mDatabaseOperationNotifier;
-private List<TripInfoModel> mAllTrips;
-private List<ExpenseModel> mAllExpenses;
+    }
 
-
-
-public DatabaseOperations_Thread(Context mContext){
-
-}
     public DatabaseOperations_Thread(Context mContext, Constants.TABLE mTableName,
                                      Constants.DATABSE_OPERATION mOperation_Name,
-                                     DatabaseOperationNotifier mDatabaseOperationNotifier){
-this.mContext=mContext;
-this.mOperation_Name= mOperation_Name;
-this.mDatabaseOperationNotifier=mDatabaseOperationNotifier;
-
-
+                                     DatabaseOperationNotifier mDatabaseOperationNotifier) {
+        this.mContext = mContext;
+        this.mOperation_Name = mOperation_Name;
+        this.mDatabaseOperationNotifier = mDatabaseOperationNotifier;
 
 
     }
+
+    public DatabaseOperations_Thread(AppCompatActivity mActivity, Constants.TABLE mTableName,
+                                     Constants.DATABSE_OPERATION mOperation_Name,
+                                     DatabaseOperationNotifier mDatabaseOperationNotifier) {
+        this.mActivity = mActivity;
+        this.mOperation_Name = mOperation_Name;
+        this.mDatabaseOperationNotifier = mDatabaseOperationNotifier;
+
+
+    }
+
     @Override
     protected Object doInBackground(Object[] objects) {
 
-    DBOperations dbOperations = new DBOperations(mContext);
+        DBOperations dbOperations = new DBOperations(mActivity);
 
-    switch (mOperation_Name)
-    {
+        switch (mOperation_Name) {
 
-        case DB_CREATE:
+            case DB_CREATE:
 
-            dbOperations.open();
-            dbOperations.close();
+                dbOperations.open();
+                dbOperations.close();
 
-            break;
-
+                break;
 
 
-        case FETCH_ALL:
+            case FETCH_ALL:
 
-            if(mTableName == Constants.TABLE.TRIPINFO ){
+                if (mTableName == Constants.TABLE.TRIPINFO) {
 
-                mAllTrips = dbOperations.getAllTrips();
+                    mAllTrips = dbOperations.getAllTrips();
 
-            }else  if(mTableName == Constants.TABLE.EXPENSE ) {
-                mAllExpenses= dbOperations.getAllExpenses();
-            }
+                } else if (mTableName == Constants.TABLE.EXPENSE) {
+                    mAllExpenses = dbOperations.getAllExpenses();
+                }
 
-            break;
-
-
+                break;
 
 
+        }
 
 
-    }
-
-
-
-
-
-            return null;
+        return null;
     }
 
 
@@ -87,7 +90,9 @@ this.mDatabaseOperationNotifier=mDatabaseOperationNotifier;
     protected void onPreExecute() {
         super.onPreExecute();
         // All the progress spinner showing goes here
-
+        mProgressDialog = new ProgressBar_YAM();
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.show(mActivity.getSupportFragmentManager(), "UI");
 
     }
 
@@ -99,21 +104,27 @@ this.mDatabaseOperationNotifier=mDatabaseOperationNotifier;
 
             case FETCH_ALL:
 
-                if(mTableName == Constants.TABLE.TRIPINFO ){
+                if (mTableName == Constants.TABLE.TRIPINFO) {
 
                     mDatabaseOperationNotifier.getTrips_INFO(mAllTrips, 0, null);
 
-                }else  if(mTableName == Constants.TABLE.EXPENSE ) {
+                } else if (mTableName == Constants.TABLE.EXPENSE) {
                     mDatabaseOperationNotifier.getExpenses_INFO(mAllExpenses, 0, null);
                 }
                 break;
 
+            case ADD_RECORD:
+
+                if (mTableName == Constants.TABLE.TRIPINFO) {
+
+
+                } else if (mTableName == Constants.TABLE.EXPENSE) {
+
+                }
+
+                break;
+
         }
-
-
-
-
-
 
 
     }
